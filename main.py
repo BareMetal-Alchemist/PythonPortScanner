28b2054780aced599eb019081a1a3020f18e5762
BareMetal-Alchemist <nscott16@student.rccd.edu>
2025-04-23 18:26:52 -0800

    add more functionality

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-import socket as sk;
+import socket
 
-ipadd = input("Enter the IP you want to scan: ")
+target = "127.0.0.1"
+
+
+service = ""
+for i in range(8079, 8082):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = s.connect((target, i))
+
+        try:
+            
+            service = s.getservbyport(i)
+            
+
+        except:
+            service = "unknown"
+        print(i, ": Port Open Service: ", service)
+    except:
+        service = "none"
+        print(i, ": Port Closed Service: ", service)
+
+    
 
 
 
-print(sk.gethostbyaddr(ipadd))
+
+
+
